1469557c5de28c1a1f6a621c70c07288eb4b9de2
alberto56 <albert@mediatribe.net>
2019-04-23 13:24:04 -0400

    Please squash this commit in the pull request

--- test.sh
+++ test.sh
@@ -4,4 +4,4 @@ docker pull dcycle/doctl:1
 docker build -t local-dcycle-doctl-image .
 
 docker run -v $(pwd)/example:/app/code dcycle/doctl:1 --help
-docker run -v $(pwd)/example:/app/code local-dcycle-doctl-image:1 --help
+docker run -v $(pwd)/example:/app/code local-dcycle-doctl-image --help
